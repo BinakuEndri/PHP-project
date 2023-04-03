6233e7a55b401066116e5ee862530f4ad2fced16
BinakuEndri <binakuendri@gmail.com>
2023-04-03 20:28:15 +0200

    delete all

--- ProjektiWeb/dashboardTemplate/html/landlord/landlord-dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php include 'landlord-menu.php' ?>
-<?php include 'landlord-navbar.php' ?>
-
-
-
-<?php include 'landlord-footer.php' ?>
--- ProjektiWeb/dashboardTemplate/html/tenant/tenant-dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php include 'tenant-menu.php' ?>
-<?php include 'tenant-navbar.php' ?>
-
-
-<?php include 'tenant-footer.php' ?>
